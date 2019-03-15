9da83cd7de5ee6dd419025e8142532d04f110d71
ashaik-jilani <45486687+ashaik-jilani@users.noreply.github.com>
2019-03-15 19:32:35 +0400

    added netflix zuul apigateway server

---
+++ src/main/java/com/supertech/microservices/netflixzuulapigatewayserver/NetflixZuulApigatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.supertech.microservices.netflixzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixZuulApigatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApigatewayServerApplication.class, args);
+	}
+
+}
